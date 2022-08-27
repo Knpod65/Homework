22605650799fabc4a0c6009ed7a43997d8313a9c
Atikarn Saengwilai <Atikarn_sa@cmu.ac.th>
2022-08-27 14:54:32 +0700

    change based on your comments.

--- Homework02.py
+++ Homework02.py
@@ -9,9 +9,9 @@ if N>0:
    print("There are",N,"sticks in the pile.")
    name=input("What is your name : ")
    print("---------------------------------")
-   c=1
+   c=0
    while N!=0:
-       if c%2==1:
+       if c%2!=1:
             if N==1 or N==2 or (N>=5 and (N-4)%3==1):
                 T=1
                 N = prc(N, T)
@@ -23,27 +23,36 @@ if N>0:
                 if T==1 or T==2:
                     N = prc(N, T)
             elif N==0:
+                print("\n")
                 break
             print("There are",N,"sticks in the pile.\n")
             c+=1
        else:
-           print(name, ", how many sticks you will take (1 or 2): ", end="")
-           t=int(input())
-           if t==1 or t==2:
-               N=N-t
-               if N==0:
-                   break
-               print("There are",N,"sticks in the pile.\n")
-           elif t>N:
-               print("There are no enough sticks to take.\n")
-           elif t==0:
-               print(name," could take it out only 1 or 2.\n")
-           elif t>2:
-               print("No! ",name," can not take more than 2 sticks!\n")
-           else:
-               print("No! ",name," can not take less than 1 sticks!\n")
-           c+=1
-   if c%2==0:
+           re=0
+           while re==0:   
+                print(name, ", how many sticks you will take (1 or 2): ", end="")
+                t=int(input())
+                if t==1 or t==2:
+                    N=N-t
+                    re=1
+                    c+=1
+                    if N==0:
+                        print("\n")
+                        break
+                    print("There are",N,"sticks in the pile.\n")
+                elif t>N:
+                    print("There are no enough sticks to take.\n")
+                    re=1
+                elif t==0:
+                    print(name," could take it out only 1 or 2.\n")
+                    re=0
+                elif t>2:
+                    print("No! ",name," can not take more than 2 sticks!\n")
+                    re=0
+                else:
+                    print("No! ",name," can not take less than 1 sticks!\n")
+                    re=0
+   if c%2==0: 
        print(name,", takes the last stick.")
        if c==1:
            print("OK. There is no stick left in the pile. We spent",c,"time together.")
